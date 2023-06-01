473323d1079050cfca456c0ac401b255cf53c6df
SolForte <mateusbrandao1234@gmail.com>
2023-06-01 13:48:21 -0300

    refactor: search posts by id

--- src/repositories/search.repository.js
+++ src/repositories/search.repository.js
@@ -36,13 +36,18 @@ async function searchPostsById(id, { limit = 20 }) {
   const { rows } = await db.query(
     `
       SELECT
-        posts.id, posts.url, posts.description, posts."userId", users.name, users.photo
+      posts.id,
+      posts.description,
+      posts.url, 
+      users.name AS "userName",
+      users.photo AS "userImageUrl",
+      users.id AS "userId"
         FROM
         posts
         JOIN users ON posts."userId" = users.id
         WHERE
         posts."userId" = $1
-        ORDER BY "createdAt"
+        ORDER BY posts."createdAt"
         DESC LIMIT $2
       ;`,
     [id, limit],
@@ -51,7 +56,9 @@ async function searchPostsById(id, { limit = 20 }) {
 }
 
 const searchRepository = {
-  searchUserName, searchUserId, searchPostsById,
+  searchUserName,
+  searchUserId,
+  searchPostsById,
 };
 
 export default searchRepository;
