4b34d75273fcea38fe349a2c7d4655a879adc75a
Victor Lahr <lahr.victor@gmail.com>
2023-06-05 02:29:47 -0300

    fix: return empty list of posts in case of unused hashtag

--- src/controllers/hashtags.controllers.js
+++ src/controllers/hashtags.controllers.js
@@ -3,8 +3,8 @@ import hashtagsRepository from '../repositories/hashtags.repository.js';
 async function retrievePosts(req, res) {
   const { hashtag } = req.params;
 
-  const valid = await hashtagsRepository.validate(hashtag);
-  if (!valid) return res.sendStatus(404);
+  // const valid = await hashtagsRepository.validate(hashtag);
+  // if (!valid) return res.sendStatus(404);
 
   try {
     const posts = await hashtagsRepository.retrievePostsBy(hashtag);
