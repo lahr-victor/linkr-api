9e0df498c101b7b096662890d303567582b44dfa
Victor Lahr <lahr.victor@gmail.com>
2023-06-09 17:02:13 -0300

    build: create new DB dump file

--- dump.sql
+++ dump.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 14.7 (Ubuntu 14.7-0ubuntu0.22.04.1)
--- Dumped by pg_dump version 14.7 (Ubuntu 14.7-0ubuntu0.22.04.1)
+-- Dumped from database version 14.8 (Ubuntu 14.8-0ubuntu0.22.04.1)
+-- Dumped by pg_dump version 14.8 (Ubuntu 14.8-0ubuntu0.22.04.1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -19,6 +19,207 @@ SET row_security = off;
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
+
+--
+-- Name: comments; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.comments (
+    id integer NOT NULL,
+    text text NOT NULL,
+    "userId" integer NOT NULL,
+    "postId" integer NOT NULL,
+    "createdAt" timestamp with time zone DEFAULT now() NOT NULL
+);
+
+
+--
+-- Name: comments_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.comments_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: comments_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.comments_id_seq OWNED BY public.comments.id;
+
+
+--
+-- Name: comments_postId_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public."comments_postId_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: comments_postId_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public."comments_postId_seq" OWNED BY public.comments."postId";
+
+
+--
+-- Name: comments_userId_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public."comments_userId_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: comments_userId_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public."comments_userId_seq" OWNED BY public.comments."userId";
+
+
+--
+-- Name: posts; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.posts (
+    id integer NOT NULL,
+    url text NOT NULL,
+    description text,
+    "userId" integer NOT NULL,
+    "createdAt" timestamp with time zone DEFAULT now() NOT NULL
+);
+
+
+--
+-- Name: reposts; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.reposts (
+    id integer NOT NULL,
+    "postId" integer NOT NULL,
+    "createdAt" timestamp with time zone DEFAULT now() NOT NULL,
+    "userId" integer NOT NULL
+);
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    name text NOT NULL,
+    photo text NOT NULL,
+    email text NOT NULL,
+    password text NOT NULL
+);
+
+
+--
+-- Name: count_reposts; Type: VIEW; Schema: public; Owner: -
+--
+
+CREATE VIEW public.count_reposts AS
+ SELECT posts.id,
+    posts.description,
+    posts.url,
+    users.name AS "userName",
+    users.photo AS "userImageUrl",
+    users.id AS "userId",
+    posts."createdAt",
+    count(reposts.id) AS "repostCount"
+   FROM ((public.posts
+     JOIN public.users ON ((users.id = posts."userId")))
+     LEFT JOIN public.reposts ON ((reposts."postId" = posts.id)))
+  GROUP BY posts.id, posts.description, posts.url, posts."createdAt", users.name, users.photo, users.id;
+
+
+--
+-- Name: follows; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.follows (
+    id integer NOT NULL,
+    "followingId" integer NOT NULL,
+    "followerId" integer NOT NULL
+);
+
+
+--
+-- Name: follows_followerId_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public."follows_followerId_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: follows_followerId_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public."follows_followerId_seq" OWNED BY public.follows."followerId";
+
+
+--
+-- Name: follows_followingId_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public."follows_followingId_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: follows_followingId_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public."follows_followingId_seq" OWNED BY public.follows."followingId";
+
+
+--
+-- Name: follows_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.follows_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: follows_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.follows_id_seq OWNED BY public.follows.id;
+
 
 --
 -- Name: hashtags; Type: TABLE; Schema: public; Owner: -
@@ -84,16 +285,45 @@ ALTER SEQUENCE public.likes_id_seq OWNED BY public.likes.id;
 
 
 --
--- Name: posts; Type: TABLE; Schema: public; Owner: -
+-- Name: posts_and_reposts; Type: VIEW; Schema: public; Owner: -
 --
 
-CREATE TABLE public.posts (
-    id integer NOT NULL,
-    url text NOT NULL,
-    description text,
-    "userId" integer NOT NULL,
-    "createdAt" timestamp with time zone DEFAULT now() NOT NULL
-);
+CREATE VIEW public.posts_and_reposts AS
+ SELECT all_posts.id,
+    all_posts.description,
+    all_posts.url,
+    all_posts."userName",
+    all_posts."userImageUrl",
+    all_posts."userId",
+    all_posts."repostCount",
+    all_posts."repostUserId",
+    all_posts."repostUserName",
+    all_posts."createdAt"
+   FROM ( SELECT cr.id,
+            cr.description,
+            cr.url,
+            cr."userName",
+            cr."userImageUrl",
+            cr."userId",
+            cr."repostCount",
+            "repostUser".id AS "repostUserId",
+            "repostUser".name AS "repostUserName",
+            reposts."createdAt"
+           FROM ((public.count_reposts cr
+             JOIN public.reposts ON ((reposts."postId" = cr.id)))
+             JOIN public.users "repostUser" ON (("repostUser".id = reposts."userId")))
+        UNION
+         SELECT cr.id,
+            cr.description,
+            cr.url,
+            cr."userName",
+            cr."userImageUrl",
+            cr."userId",
+            cr."repostCount",
+            NULL::integer AS "repostUserId",
+            NULL::text AS "repostUserName",
+            cr."createdAt"
+           FROM public.count_reposts cr) all_posts;
 
 
 --
@@ -114,6 +344,46 @@ CREATE SEQUENCE public.posts_id_seq
 --
 
 ALTER SEQUENCE public.posts_id_seq OWNED BY public.posts.id;
+
+
+--
+-- Name: reposts_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.reposts_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: reposts_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.reposts_id_seq OWNED BY public.reposts.id;
+
+
+--
+-- Name: reposts_postId_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public."reposts_postId_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: reposts_postId_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public."reposts_postId_seq" OWNED BY public.reposts."postId";
 
 
 --
@@ -148,19 +418,6 @@ ALTER SEQUENCE public.sessions_id_seq OWNED BY public.sessions.id;
 
 
 --
--- Name: users; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.users (
-    id integer NOT NULL,
-    name text NOT NULL,
-    photo text NOT NULL,
-    email text NOT NULL,
-    password text NOT NULL
-);
-
-
---
 -- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -178,6 +435,48 @@ CREATE SEQUENCE public.users_id_seq
 --
 
 ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- Name: comments id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.comments ALTER COLUMN id SET DEFAULT nextval('public.comments_id_seq'::regclass);
+
+
+--
+-- Name: comments userId; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.comments ALTER COLUMN "userId" SET DEFAULT nextval('public."comments_userId_seq"'::regclass);
+
+
+--
+-- Name: comments postId; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.comments ALTER COLUMN "postId" SET DEFAULT nextval('public."comments_postId_seq"'::regclass);
+
+
+--
+-- Name: follows id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.follows ALTER COLUMN id SET DEFAULT nextval('public.follows_id_seq'::regclass);
+
+
+--
+-- Name: follows followingId; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.follows ALTER COLUMN "followingId" SET DEFAULT nextval('public."follows_followingId_seq"'::regclass);
+
+
+--
+-- Name: follows followerId; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.follows ALTER COLUMN "followerId" SET DEFAULT nextval('public."follows_followerId_seq"'::regclass);
 
 
 --
@@ -202,6 +501,20 @@ ALTER TABLE ONLY public.posts ALTER COLUMN id SET DEFAULT nextval('public.posts_
 
 
 --
+-- Name: reposts id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.reposts ALTER COLUMN id SET DEFAULT nextval('public.reposts_id_seq'::regclass);
+
+
+--
+-- Name: reposts postId; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.reposts ALTER COLUMN "postId" SET DEFAULT nextval('public."reposts_postId_seq"'::regclass);
+
+
+--
 -- Name: sessions id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -213,6 +526,20 @@ ALTER TABLE ONLY public.sessions ALTER COLUMN id SET DEFAULT nextval('public.ses
 --
 
 ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Data for Name: comments; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+
+
+--
+-- Data for Name: follows; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO public.follows VALUES (17, 8, 4);
+INSERT INTO public.follows VALUES (18, 5, 4);
 
 
 --
@@ -272,13 +599,6 @@ INSERT INTO public.likes VALUES (46, 28, 9);
 INSERT INTO public.likes VALUES (47, 26, 9);
 INSERT INTO public.likes VALUES (48, 41, 8);
 INSERT INTO public.likes VALUES (49, 37, 8);
-INSERT INTO public.likes VALUES (52, 38, 3);
-INSERT INTO public.likes VALUES (53, 30, 3);
-INSERT INTO public.likes VALUES (54, 26, 3);
-INSERT INTO public.likes VALUES (55, 32, 3);
-INSERT INTO public.likes VALUES (56, 40, 3);
-INSERT INTO public.likes VALUES (57, 43, 3);
-INSERT INTO public.likes VALUES (58, 28, 3);
 
 
 --
@@ -299,6 +619,12 @@ INSERT INTO public.posts VALUES (43, 'https://www.imdb.com/title/tt0077651/?ref_
 
 
 --
+-- Data for Name: reposts; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+
+
+--
 -- Data for Name: sessions; Type: TABLE DATA; Schema: public; Owner: -
 --
 
@@ -308,7 +634,6 @@ INSERT INTO public.posts VALUES (43, 'https://www.imdb.com/title/tt0077651/?ref_
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.users VALUES (3, 'Victor Lahr', 'https://noticiasdatv.uol.com.br/media/_versions/artigos_2021/o-exorcista-1973-linda-blair-warner-bros_fixed_large.jpg', 'lahr.victor@gmail.com', '$2b$10$0dPH2PMUVdmX5ux9ab25MuVm1s2gCx1XFZwkXl.Jxi1/890sQysCO');
 INSERT INTO public.users VALUES (4, 'Freddy Krueger', 'https://rollingstone.uol.com.br/media/uploads/2023/05/freddy-krueger-personagem-ator-robert-englund-foto-divulgacao.jpg', 'freddy.krueger@gmail.com', '$2b$10$zeK.qOX/oTURbm4Thsb9iuUk9HDke/53p0YHc6s5opJKWVp.VKn6W');
 INSERT INTO public.users VALUES (5, 'Jason Voorhees', 'https://cinevicio.com.br/wp-content/uploads/2022/11/image-1-20.jpg', 'jason.voorhess@gmail.com', '$2b$10$AYVLTWtBEvaxEt9jqMb2y.yc39xqO.RkN51dBpFJCoUjwcrakzDX2');
 INSERT INTO public.users VALUES (6, 'Michael Myers', 'https://pbs.twimg.com/profile_images/1607454917244981249/hixabtds_400x400.jpg', 'michael.myers@gmail.com', '$2b$10$vqV2F.ujx6f0vHp5Dyq4Hu91Cp.bZ8gn6owTXVoUam8Fxmy531a9.');
@@ -318,38 +643,118 @@ INSERT INTO public.users VALUES (10, 'Pinhead', 'https://1.bp.blogspot.com/-TUOn
 
 
 --
+-- Name: comments_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.comments_id_seq', 27, true);
+
+
+--
+-- Name: comments_postId_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public."comments_postId_seq"', 1, false);
+
+
+--
+-- Name: comments_userId_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public."comments_userId_seq"', 1, false);
+
+
+--
+-- Name: follows_followerId_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public."follows_followerId_seq"', 1, false);
+
+
+--
+-- Name: follows_followingId_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public."follows_followingId_seq"', 1, false);
+
+
+--
+-- Name: follows_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.follows_id_seq', 18, true);
+
+
+--
 -- Name: hashtags_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.hashtags_id_seq', 57, true);
+SELECT pg_catalog.setval('public.hashtags_id_seq', 1, false);
 
 
 --
 -- Name: likes_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.likes_id_seq', 58, true);
+SELECT pg_catalog.setval('public.likes_id_seq', 2, true);
 
 
 --
 -- Name: posts_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.posts_id_seq', 43, true);
+SELECT pg_catalog.setval('public.posts_id_seq', 1, false);
+
+
+--
+-- Name: reposts_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.reposts_id_seq', 1, false);
+
+
+--
+-- Name: reposts_postId_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public."reposts_postId_seq"', 1, false);
 
 
 --
 -- Name: sessions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.sessions_id_seq', 23, true);
+SELECT pg_catalog.setval('public.sessions_id_seq', 2, true);
 
 
 --
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 10, true);
+SELECT pg_catalog.setval('public.users_id_seq', 1, false);
+
+
+--
+-- Name: comments comments_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.comments
+    ADD CONSTRAINT comments_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: follows follows_followingId_followerId_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.follows
+    ADD CONSTRAINT "follows_followingId_followerId_key" UNIQUE ("followingId", "followerId");
+
+
+--
+-- Name: follows follows_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.follows
+    ADD CONSTRAINT follows_pkey PRIMARY KEY (id);
 
 
 --
@@ -385,11 +790,27 @@ ALTER TABLE ONLY public.posts
 
 
 --
+-- Name: reposts reposts_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.reposts
+    ADD CONSTRAINT reposts_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: sessions sessions_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.sessions
     ADD CONSTRAINT sessions_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: reposts user_post_unique; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.reposts
+    ADD CONSTRAINT user_post_unique UNIQUE ("userId", "postId");
 
 
 --
@@ -423,6 +844,38 @@ CREATE INDEX hashtags_fk ON public.hashtags USING btree ("postId");
 
 
 --
+-- Name: comments comments_postId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.comments
+    ADD CONSTRAINT "comments_postId_fkey" FOREIGN KEY ("postId") REFERENCES public.posts(id) ON DELETE CASCADE NOT VALID;
+
+
+--
+-- Name: comments comments_userId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.comments
+    ADD CONSTRAINT "comments_userId_fkey" FOREIGN KEY ("userId") REFERENCES public.users(id) NOT VALID;
+
+
+--
+-- Name: follows follows_followerId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.follows
+    ADD CONSTRAINT "follows_followerId_fkey" FOREIGN KEY ("followerId") REFERENCES public.users(id) NOT VALID;
+
+
+--
+-- Name: follows follows_followingId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.follows
+    ADD CONSTRAINT "follows_followingId_fkey" FOREIGN KEY ("followingId") REFERENCES public.users(id) NOT VALID;
+
+
+--
 -- Name: hashtags hashtags_postId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -452,6 +905,22 @@ ALTER TABLE ONLY public.likes
 
 ALTER TABLE ONLY public.posts
     ADD CONSTRAINT "posts_userId_fkey" FOREIGN KEY ("userId") REFERENCES public.users(id) NOT VALID;
+
+
+--
+-- Name: reposts reposts_postId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.reposts
+    ADD CONSTRAINT "reposts_postId_fkey" FOREIGN KEY ("postId") REFERENCES public.posts(id) ON DELETE CASCADE NOT VALID;
+
+
+--
+-- Name: reposts reposts_userId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.reposts
+    ADD CONSTRAINT "reposts_userId_fkey" FOREIGN KEY ("userId") REFERENCES public.users(id);
 
 
 --
