5b3e9b5ab022c2af06bd5a0a56a4c100b138ca1e
Victor Lahr <lahr.victor@gmail.com>
2023-06-09 18:57:02 -0300

    build: create new DB dump file

--- dump.sql
+++ dump.sql
@@ -538,84 +538,24 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 -- Data for Name: follows; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.follows VALUES (17, 8, 4);
-INSERT INTO public.follows VALUES (18, 5, 4);
 
 
 --
 -- Data for Name: hashtags; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.hashtags VALUES (19, 'WelcomeToMyNightmare', '2023-06-05 01:12:10.445575-03', 26);
-INSERT INTO public.hashtags VALUES (20, 'ImYourBoyfriendNowNancy', '2023-06-05 01:12:10.448489-03', 26);
-INSERT INTO public.hashtags VALUES (23, 'KillKillKillHaHaHa', '2023-06-05 01:17:13.764429-03', 28);
-INSERT INTO public.hashtags VALUES (24, 'ChhChhChhAhAhAh', '2023-06-05 01:17:13.765658-03', 28);
-INSERT INTO public.hashtags VALUES (27, 'WelcomeToMyNightmare', '2023-06-05 01:20:43.036262-03', 30);
-INSERT INTO public.hashtags VALUES (28, 'FreddysComingForYou', '2023-06-05 01:20:43.037484-03', 30);
-INSERT INTO public.hashtags VALUES (31, 'TheNightHeCameHome', '2023-06-05 01:26:46.045369-03', 32);
-INSERT INTO public.hashtags VALUES (32, 'EvilNeverDies', '2023-06-05 01:26:46.047781-03', 32);
-INSERT INTO public.hashtags VALUES (33, 'TheDevilsEyes', '2023-06-05 01:26:46.049265-03', 32);
-INSERT INTO public.hashtags VALUES (37, 'TheSawIsFamily', '2023-06-05 01:30:47.310392-03', 34);
-INSERT INTO public.hashtags VALUES (38, 'TheFamilyThatSlaysTogether', '2023-06-05 01:30:47.31214-03', 34);
-INSERT INTO public.hashtags VALUES (39, 'IWillStayAndBeBeautiful', '2023-06-05 01:30:47.313182-03', 34);
-INSERT INTO public.hashtags VALUES (46, 'WannaPlay', '2023-06-05 01:34:12.918001-03', 37);
-INSERT INTO public.hashtags VALUES (47, 'WellTearYourSoulApart', '2023-06-05 01:36:08.222623-03', 38);
-INSERT INTO public.hashtags VALUES (51, 'IWillStayAndBeBeautiful', '2023-06-05 01:53:51.657954-03', 40);
-INSERT INTO public.hashtags VALUES (52, 'WannaPlay', '2023-06-05 01:55:46.753811-03', 41);
-INSERT INTO public.hashtags VALUES (53, 'IllBeBackIAlwaysComeBack', '2023-06-05 01:55:46.755411-03', 41);
-INSERT INTO public.hashtags VALUES (54, 'WellTearYourSoulApart', '2023-06-05 01:56:35.203462-03', 42);
-INSERT INTO public.hashtags VALUES (55, 'YourSufferingWillBeLegendary', '2023-06-05 01:56:35.204928-03', 42);
-INSERT INTO public.hashtags VALUES (56, 'TheNightHeCameHome', '2023-06-05 01:57:37.954885-03', 43);
-INSERT INTO public.hashtags VALUES (57, 'EvilNeverDies', '2023-06-05 01:57:37.956175-03', 43);
 
 
 --
 -- Data for Name: likes; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.likes VALUES (24, 42, 6);
-INSERT INTO public.likes VALUES (25, 40, 6);
-INSERT INTO public.likes VALUES (26, 34, 6);
-INSERT INTO public.likes VALUES (27, 28, 6);
-INSERT INTO public.likes VALUES (28, 43, 4);
-INSERT INTO public.likes VALUES (29, 42, 4);
-INSERT INTO public.likes VALUES (30, 41, 4);
-INSERT INTO public.likes VALUES (31, 40, 4);
-INSERT INTO public.likes VALUES (32, 38, 4);
-INSERT INTO public.likes VALUES (33, 37, 4);
-INSERT INTO public.likes VALUES (34, 34, 4);
-INSERT INTO public.likes VALUES (35, 32, 4);
-INSERT INTO public.likes VALUES (36, 28, 4);
-INSERT INTO public.likes VALUES (37, 32, 5);
-INSERT INTO public.likes VALUES (38, 43, 5);
-INSERT INTO public.likes VALUES (39, 43, 9);
-INSERT INTO public.likes VALUES (40, 42, 9);
-INSERT INTO public.likes VALUES (41, 40, 9);
-INSERT INTO public.likes VALUES (42, 38, 9);
-INSERT INTO public.likes VALUES (43, 34, 9);
-INSERT INTO public.likes VALUES (44, 32, 9);
-INSERT INTO public.likes VALUES (45, 30, 9);
-INSERT INTO public.likes VALUES (46, 28, 9);
-INSERT INTO public.likes VALUES (47, 26, 9);
-INSERT INTO public.likes VALUES (48, 41, 8);
-INSERT INTO public.likes VALUES (49, 37, 8);
 
 
 --
 -- Data for Name: posts; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.posts VALUES (26, 'https://www.imdb.com/title/tt0087800/?ref_=ext_shr_lnk', '#WelcomeToMyNightmare #ImYourBoyfriendNowNancy', 4, '2023-06-05 01:12:10.441667-03');
-INSERT INTO public.posts VALUES (28, 'https://www.imdb.com/title/tt0080761/?ref_=ext_shr_lnk', '#KillKillKillHaHaHa #ChhChhChhAhAhAh', 5, '2023-06-05 01:17:13.762554-03');
-INSERT INTO public.posts VALUES (30, 'https://www.imdb.com/title/tt0093629/?ref_=ext_shr_lnk', '#WelcomeToMyNightmare #FreddysComingForYou', 4, '2023-06-05 01:20:43.034387-03');
-INSERT INTO public.posts VALUES (32, 'https://www.imdb.com/title/tt1502407/?ref_=ext_shr_lnk', '#TheNightHeCameHome #EvilNeverDies #TheDevilsEyes', 6, '2023-06-05 01:26:46.043113-03');
-INSERT INTO public.posts VALUES (34, 'https://www.imdb.com/title/tt0072271/?ref_=ext_shr_lnk', '#TheSawIsFamily #TheFamilyThatSlaysTogether #IWillStayAndBeBeautiful', 8, '2023-06-05 01:30:47.306157-03');
-INSERT INTO public.posts VALUES (37, 'https://www.imdb.com/title/tt8388390/?ref_=ext_shr_lnk', '#WannaPlay', 9, '2023-06-05 01:34:12.916259-03');
-INSERT INTO public.posts VALUES (38, 'https://www.imdb.com/title/tt0093177/?ref_=ext_shr_lnk', '#WellTearYourSoulApart', 10, '2023-06-05 01:36:08.220898-03');
-INSERT INTO public.posts VALUES (40, 'https://www.imdb.com/title/tt0324216/?ref_=ext_shr_lnk', '#IWillStayAndBeBeautiful', 8, '2023-06-05 01:53:51.654926-03');
-INSERT INTO public.posts VALUES (41, 'https://www.imdb.com/title/tt0094862/?ref_=ext_shr_lnk', '#WannaPlay #IllBeBackIAlwaysComeBack', 9, '2023-06-05 01:55:46.751607-03');
-INSERT INTO public.posts VALUES (42, 'https://www.imdb.com/title/tt0095294/?ref_=ext_shr_lnk', '#WellTearYourSoulApart #YourSufferingWillBeLegendary', 10, '2023-06-05 01:56:35.20118-03');
-INSERT INTO public.posts VALUES (43, 'https://www.imdb.com/title/tt0077651/?ref_=ext_shr_lnk', '#TheNightHeCameHome #EvilNeverDies', 6, '2023-06-05 01:57:37.953142-03');
 
 
 --
@@ -634,12 +574,6 @@ INSERT INTO public.posts VALUES (43, 'https://www.imdb.com/title/tt0077651/?ref_
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.users VALUES (4, 'Freddy Krueger', 'https://rollingstone.uol.com.br/media/uploads/2023/05/freddy-krueger-personagem-ator-robert-englund-foto-divulgacao.jpg', 'freddy.krueger@gmail.com', '$2b$10$zeK.qOX/oTURbm4Thsb9iuUk9HDke/53p0YHc6s5opJKWVp.VKn6W');
-INSERT INTO public.users VALUES (5, 'Jason Voorhees', 'https://cinevicio.com.br/wp-content/uploads/2022/11/image-1-20.jpg', 'jason.voorhess@gmail.com', '$2b$10$AYVLTWtBEvaxEt9jqMb2y.yc39xqO.RkN51dBpFJCoUjwcrakzDX2');
-INSERT INTO public.users VALUES (6, 'Michael Myers', 'https://pbs.twimg.com/profile_images/1607454917244981249/hixabtds_400x400.jpg', 'michael.myers@gmail.com', '$2b$10$vqV2F.ujx6f0vHp5Dyq4Hu91Cp.bZ8gn6owTXVoUam8Fxmy531a9.');
-INSERT INTO public.users VALUES (8, 'Leatherface', 'https://upload.wikimedia.org/wikipedia/en/9/94/Leatherface%2C_The_Texas_Chain_Saw_Massacre%2C_1974%2C_Colorized.jpg', 'leatherface@gmail.com', '$2b$10$BbhSzQB4WUYfJfbXz9BXWuufN5isEiBUuLadPMYk3ZmvMriRh7ySq');
-INSERT INTO public.users VALUES (9, 'Chucky', 'https://uploads.jovemnerd.com.br/wp-content/uploads/2022/10/chucky_segunda_temporada_estreia__x64bel-1210x544.jpg', 'chucky@gmail.com', '$2b$10$Dj21v9CeZ6hh5FLMk68T8eeGKaY4FMqv6on32nsGY7jeiBt5EwSMm');
-INSERT INTO public.users VALUES (10, 'Pinhead', 'https://1.bp.blogspot.com/-TUOnC0pk3EQ/YX6nN78CuGI/AAAAAAAALIE/0Qc20jAkfMAgArcV_DYAx7URi0Mo_yrLACLcBGAsYHQ/s540/pinhead%2Bhellraiser.jpg', 'pinhead@gmail.com', '$2b$10$3VMMsfgCrqQqogNovvA98OOqMWFh3JZFe1coxixHsvL7WcO0sDGXO');
 
 
 --
