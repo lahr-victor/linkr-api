cabb111485ffb068511c8d36d10970ad8191654d
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-06-09 16:53:51 -0300

    fix: query of search posts by userId getting posts and reposts

--- src/routes/search.routes.js
+++ src/routes/search.routes.js
@@ -1,11 +1,26 @@
 import { Router } from 'express';
 import searchControllers from '../controllers/search.controllers.js';
 import authValidation from '../middlewares/authValidation.middleware.js';
+import schemaMiddleware from '../middlewares/schema.middleware.js';
+import postSchema from '../schemas/post.schema.js';
 
 const searchRouter = Router();
 
-searchRouter.get('/search/user/:id', authValidation, searchControllers.getUserById);
-searchRouter.get('/search/user', authValidation, searchControllers.searchUsersByName);
-searchRouter.get('/search/posts/:id', authValidation, searchControllers.searchPostsById);
+searchRouter.get(
+  '/search/user/:id',
+  authValidation,
+  searchControllers.getUserById,
+);
+searchRouter.get(
+  '/search/user',
+  authValidation,
+  searchControllers.searchUsersByName,
+);
+searchRouter.get(
+  '/search/posts/:id',
+  schemaMiddleware.validateQuery(postSchema.query),
+  authValidation,
+  searchControllers.searchPostsById,
+);
 
 export default searchRouter;
